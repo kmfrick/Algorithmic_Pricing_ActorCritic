a32cad265a286099b1a79097d1f85e2704dc5ee9
kmfrick <kmfrick98@gmail.com>
2022-04-13 01:48:56 +0200

    Save and plot actions and values

--- main.py
+++ main.py
@@ -4,15 +4,11 @@ import copy
 import os
 import random
 import sys
-import traceback
-import time
 
 from collections import deque
 from tqdm import tqdm
 
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
 import torch
 import torch.nn as nn
@@ -20,72 +16,59 @@ import torch.nn.functional as F
 import torch.distributions as D
 
 np.random.seed(50321)
+
+
 def fanin_init(size, fanin=None):
     fanin = fanin or size[0]
     v = 1. / np.sqrt(fanin)
     return torch.Tensor(size).uniform_(-v, v)
 
-class ActorCriticNetwork(nn.Module):
 
+"""
+Actor-Critic NN
+Adapted from https://github.com/vy007vikas/PyTorch-ActorCriticRL
+"""
+
+
+class ActorCriticNetwork(nn.Module):
     def __init__(self, state_dim):
-        EPS=3e-3
+        EPS = 3e-3
         super(ActorCriticNetwork, self).__init__()
         self.state_dim = state_dim
         self.action_lim = 1
-
-        self.fcs1 = nn.Linear(state_dim,256)
+        # Critic layers
+        self.fcs1 = nn.Linear(state_dim, 256)
         self.fcs1.weight.data = fanin_init(self.fcs1.weight.data.size())
-        self.fcs2 = nn.Linear(256,128)
+        self.fcs2 = nn.Linear(256, 128)
         self.fcs2.weight.data = fanin_init(self.fcs2.weight.data.size())
-
-        self.fca1 = nn.Linear(1,128)
+        self.fca1 = nn.Linear(1, 128)
         self.fca1.weight.data = fanin_init(self.fca1.weight.data.size())
-
-        self.fc_critic2 = nn.Linear(256,128)
+        self.fc_critic2 = nn.Linear(256, 128)
         self.fc_critic2.weight.data = fanin_init(self.fc_critic2.weight.data.size())
-
-        self.fc_critic3 = nn.Linear(128,1)
-        self.fc_critic3.weight.data.uniform_(-EPS,EPS)
-
-        self.fc_actor1 = nn.Linear(state_dim,256)
+        self.fc_critic3 = nn.Linear(128, 1)
+        self.fc_critic3.weight.data.uniform_(-EPS, EPS)
+        # Actor layers
+        self.fc_actor1 = nn.Linear(state_dim, 256)
         self.fc_actor1.weight.data = fanin_init(self.fc_actor1.weight.data.size())
-
-        self.fc_actor2 = nn.Linear(256,128)
+        self.fc_actor2 = nn.Linear(256, 128)
         self.fc_actor2.weight.data = fanin_init(self.fc_actor2.weight.data.size())
-
-        self.fc_actor3 = nn.Linear(128,64)
+        self.fc_actor3 = nn.Linear(128, 64)
         self.fc_actor3.weight.data = fanin_init(self.fc_actor3.weight.data.size())
-
-        self.fc_actor4 = nn.Linear(64,1)
-        self.fc_actor4.weight.data.uniform_(-EPS,EPS)
+        self.fc_actor4 = nn.Linear(64, 1)
+        self.fc_actor4.weight.data.uniform_(-EPS, EPS)
 
     def critic(self, state, action):
-        """
-        returns Value function Q(s,a) obtained from critic network
-        :param state: Input state (Torch Variable : [n,state_dim] )
-        :param action: Input Action (Torch Variable : [n,1] )
-        :return: Value function : Q(S,a) (Torch Variable : [n,1] )
-        """
         state = state.float()
         action = action.float()
         s1 = F.relu(self.fcs1(state))
         s2 = F.relu(self.fcs2(s1))
         a1 = F.relu(self.fca1(action))
-        x = torch.cat((s2,a1),dim=1)
+        x = torch.cat((s2, a1), dim=1)
         x = F.relu(self.fc_critic2(x))
         x = self.fc_critic3(x)
         return x
 
-
     def actor(self, state):
-        """
-        returns policy function Pi(s) obtained from actor network
-        this function is a gaussian prob distribution for all actions
-        with mean lying in (-1,1) and sigma lying in (0,1)
-        The sampled action can , then later be rescaled
-        :param state: Input state (Torch Variable : [n,state_dim] )
-        :return: Output action (Torch Variable: [n,1] )
-        """
         state = state.float()
         x = F.relu(self.fc_actor1(state))
         x = F.relu(self.fc_actor2(x))
@@ -100,30 +83,72 @@ class ActorCriticNetwork(nn.Module):
         return a, v
 
 
+def compute_profit(ai, a0, mu, c, p):
+    q = np.exp((ai - p) / mu) / (np.sum(np.exp((ai - p) / mu)) + np.exp(a0 / mu))
+    pi = (p - c) * q
+    return pi
+
+
+def compute_profit_derivative(ai, a0, mu, c, p, h):
+    def f(price):
+        return compute_profit(ai, a0, mu, c, price)
+
+    return (f(p + h) - f(p - h)) / (2 * h)
+
+
+def optimize(batch_size, buffer, net, optimizer, scheduler, target_net):
+    DISCOUNT = 0.95
+    TARGET_LR = 0.001
+    batch = random.sample(buffer, min(len(buffer), batch_size))
+    s = torch.tensor(np.float32([arr[0] for arr in batch]))
+    a = torch.tensor(np.float32([arr[1] for arr in batch]))
+    r = torch.tensor(np.float32([arr[2] for arr in batch]))
+    s1 = torch.tensor(np.float32([arr[3] for arr in batch]))
+    a1 = target_net.actor(s1).detach()
+    next_val = target_net.critic(s1, a1).detach().squeeze()
+    y_exp = r + DISCOUNT * next_val
+    y_pred = net.critic(s, a.unsqueeze(0).mT).squeeze()
+    l_critic = F.smooth_l1_loss(y_pred, y_exp)
+    _, pred_v = net(s)
+    l_actor = -torch.sum(pred_v)
+    loss = l_critic + l_actor
+    optimizer.zero_grad()
+    loss.backward()
+    torch.nn.utils.clip_grad_norm_(net.parameters(), 1)
+    torch.nn.utils.clip_grad_norm_(target_net.parameters(), 1)
+    optimizer.step()
+    scheduler.step()
+    total_norm = 0
+    for target_param, param in zip(target_net.parameters(), net.parameters()):
+        target_param.data.copy_(target_param.data * (1.0 - TARGET_LR) + param.data * TARGET_LR)
+        param_norm = param.grad.detach().data.norm(2)
+        total_norm += param_norm.item() ** 2
+    return scheduler.get_last_lr()[0], total_norm
+
+
 def main():
     if len(sys.argv) < 2:
         print(f"Usage: {sys.argv[0]} FOLDER_NAME")
         exit(1)
     else:
         out_dir = sys.argv[1]
-        os.makedirs(out_dir, exist_ok = True)
-    discount = 0.95
-    n_episodes = 100
+        os.makedirs(out_dir, exist_ok=True)
     n_agents = 2
     ai = np.array([2.0, 2.0])
     a0 = 0
     mu = 0.25
     c = 1
-    batch_size = 128
-    initial_lr = 3e-4
-    tau = 0.001
-    lr_decay = 0.995
-    nash_price = 1.47
-    beta = 3e-3
-    seeds = [12345]#54321, 50321, 9827391, 8534101, 4305430, 12654329, 3483055, 348203, 2356, 250917, 200822, 151515, 50505, 301524]
-    max_t = int(1e3)
+    BATCH_SIZE = 128
+    INITIAL_LR = 3e-4
+    LR_DECAY = 0.99
+    EXPLORATION_DECAY = 3e-3
+    MAX_T = int(1e3)
+    nash_price = 1.47293
+    seeds = [12345, 54321, 50321, 9827391, 8534101, 4305430, 12654329, 3483055, 348203, 2356, 250917, 200822, 151515,
+             50505, 301524]
+
     ir_periods = 20
-    #torch.autograd.set_detect_anomaly(True)
+    # torch.autograd.set_detect_anomaly(True)
     for session in range(len(seeds)):
         # Random seeds
         np.random.seed(seeds[session])
@@ -133,79 +158,65 @@ def main():
         net = []
         for i in range(n_agents):
             net.append(ActorCriticNetwork(n_agents))
-            buffer.append(deque(maxlen=int(max_t / 10)))
+            buffer.append(deque(maxlen=BATCH_SIZE * 2))
         # Do a deepcopy of the NN to ensure target networks are initialized the same way
         target_net = copy.deepcopy(net)
         # One optimizer per network
-        optimizer = [torch.optim.AdamW(n.parameters(), lr=initial_lr) for n in net]
+        optimizer = [torch.optim.AdamW(n.parameters(), lr=INITIAL_LR) for n in net]
         # One scheduler per network
-        scheduler = [torch.optim.lr_scheduler.ExponentialLR(o, gamma = lr_decay) for o in optimizer]
+        scheduler = [torch.optim.lr_scheduler.ExponentialLR(o, gamma=LR_DECAY) for o in optimizer]
         # Initial state is random, but ensure prices are above marginal cost
         state = torch.sigmoid(torch.randn(n_agents)) + c
         price = np.zeros([n_agents])
-        total_reward = np.zeros([n_agents, max_t])
-        price_history = np.zeros([n_agents, max_t])
-        collusive_t = 0
-        for t in tqdm(range(max_t)):
+        total_reward = np.zeros([n_agents, MAX_T])
+        price_history = np.zeros([n_agents, MAX_T])
+        grad_norm = np.zeros([n_agents, MAX_T])
+        net_lr = np.zeros([n_agents, MAX_T])
+        for t in tqdm(range(MAX_T)):
             for i in range(n_agents):
                 p = net[i].actor(state)
-                price[i] = (p + c + D.Normal(0, np.exp(- beta * t)).sample()).detach().numpy()
+                price[i] = (p + c + D.Normal(0, np.exp(- EXPLORATION_DECAY * t)).sample()).detach().numpy()
                 price_history[i, t] = price[i]
-            quant = np.exp((ai - price)/mu) / (np.sum(np.exp((ai - price)/mu)) + np.exp(a0/mu))
-            profits = (price - c) * quant
+            profits = compute_profit(ai, a0, mu, c, price)
             for i in range(n_agents):
                 transition = state.detach().numpy(), price[i], profits[i], price
                 buffer[i].append(transition)
                 total_reward[i, t] = profits[i]
             state = torch.tensor(price)
-            if t > batch_size:
+            if t > 0:  # At least one experience in the buffer
                 for i in range(n_agents):
-                    batch = []
-                    batch = random.sample(buffer[i], min(len(buffer[i]), batch_size))
-                    s = torch.tensor(np.float32([arr[0] for arr in batch]))
-                    a = torch.tensor(np.float32([arr[1] for arr in batch]))
-                    r = torch.tensor(np.float32([arr[2] for arr in batch]))
-                    s1 = torch.tensor(np.float32([arr[3] for arr in batch]))
-                    a1 = target_net[i].actor(s1).detach()
-                    next_val = target_net[i].critic(s1, a1).detach().squeeze()
-                    y_exp = r + discount * next_val
-                    y_pred = net[i].critic(s, a.unsqueeze(0).mT).squeeze()
-                    l_critic = F.mse_loss(y_pred, y_exp)
-                    _, pred_v = net[i](s)
-                    l_actor = -torch.sum(pred_v)
-                    l = l_critic + l_actor
-                    optimizer[i].zero_grad()
-                    l.backward()
-                    optimizer[i].step()
-                    scheduler[i].step()
-                    for target_param, param in zip(target_net[i].parameters(), net[i].parameters()):
-                        target_param.data.copy_(
-                            target_param.data * (1.0 - tau) + param.data * tau
-                        )
+                    nlr, gn = optimize(BATCH_SIZE, buffer[i], net[i], optimizer[i], scheduler[i], target_net[i])
+                    net_lr[i, t] = nlr
+                    grad_norm[i, t] = gn
         np.save(f"{out_dir}/session_reward_{seeds[session]}.npy", total_reward)
+        np.save(f"{out_dir}/grad_norm_{seeds[session]}.npy", grad_norm)
+        np.save(f"{out_dir}/net_lr_{seeds[session]}.npy", net_lr)
+        # Impulse response
         ir_profits = np.zeros([n_agents, ir_periods])
         ir_prices = np.zeros([n_agents, ir_periods])
-        # Impulse response
         for t in range(ir_periods):
             for i in range(n_agents):
                 p = net[i].actor(state)
                 price[i] = (p + c).detach().numpy()
                 ir_prices[i, t] = price[i]
-            if t == (ir_periods / 2):
+            if (ir_periods / 2) <= t <= (ir_periods / 2 + 3):
                 price[0] = nash_price
-                ir_prices[0, t] = nash_price
-            quant = np.exp((ai - price)/mu) / (np.sum(np.exp((ai - price)/mu)) + np.exp(a0/mu))
-            ir_profits[:, t] = (price - c) * quant
+                ir_prices[0, t] = price[0]
+            ir_profits[:, t] = compute_profit(ai, a0, mu, c, price)
             state = torch.tensor(price)
         np.save(f"{out_dir}/ir_profits_{seeds[session]}.npy", ir_profits)
         np.save(f"{out_dir}/ir_prices_{seeds[session]}.npy", ir_prices)
-
-
-
-
-
-
-
+        # state-action mappings
+        w = np.linspace(c, c + 2, 100)
+        x = np.linspace(c, c + 2, 100)
+        W = np.transpose([np.repeat(w, len(x)), np.tile(x, len(w))])
+        for i in range(n_agents):
+            # FIXME: Check the layout of what is saved, it's probably rotated wrong
+            a, q = net[i](torch.tensor(W))
+            a = a.detach().numpy()
+            q = q.detach().numpy()
+            np.save(f"{out_dir}/actions_{seeds[session]}_{i}.npy", a)
+            np.save(f"{out_dir}/values_{seeds[session]}_{i}.npy", a)
 
 
 if __name__ == "__main__":
--- plot.py
+++ plot.py
@@ -6,9 +6,9 @@ import pandas as pd
 import sys
 import os
 
-def plot_reward_array(rewards, nash, coop, title, rolling_interval, fnames, points = False):
-    tot0 = np.sum(np.array([r[0] for r in rewards]), axis = 0) / len(rewards)
-    tot1 = np.sum(np.array([r[1] for r in rewards]), axis = 0) / len(rewards)
+def plot_array_rolling_avg(arr, low_line, high_line, title, rolling_interval, fnames, points = False):
+    tot0 = np.sum(np.array([r[0] for r in arr]), axis = 0) / len(arr)
+    tot1 = np.sum(np.array([r[1] for r in arr]), axis = 0) / len(arr)
     tot0_series = pd.Series(tot0).rolling(rolling_interval).mean()
     tot1_series = pd.Series(tot1).rolling(rolling_interval).mean()
     if (points):
@@ -16,11 +16,11 @@ def plot_reward_array(rewards, nash, coop, title, rolling_interval, fnames, poin
         plt.scatter(list(range(len(tot1_series))), tot1_series)
     plt.plot(tot0_series)
     plt.plot(tot1_series)
-    plt.axhline(nash)
-    plt.axhline(coop)
+    plt.axhline(low_line)
+    plt.axhline(high_line)
     plt.title(f"Average {title}")
     plt.show()
-    for i, r in enumerate(rewards):
+    for i, r in enumerate(arr):
         r0_series = pd.Series(r[0]).rolling(rolling_interval).mean()
         r1_series = pd.Series(r[1]).rolling(rolling_interval).mean()
         if (points):
@@ -28,9 +28,19 @@ def plot_reward_array(rewards, nash, coop, title, rolling_interval, fnames, poin
             plt.scatter(list(range(len(r1_series))), r1_series)
         plt.plot(r0_series)
         plt.plot(r1_series)
-        plt.axhline(nash)
-        plt.axhline(coop)
+        plt.axhline(low_line)
+        plt.axhline(high_line)
         plt.title(f"Session {i} {title}: {fnames[i]}")
+        plt.show()
+
+def plot_heatmap(arr, title, fnames):
+    for i, a in enumerate(arr):
+        ax = plt.subplot()
+        im = ax.imshow(a.reshape([100, 100]), cmap="Reds", extent=(1, 3, 1, 3), aspect="auto")
+        ax.set_xlabel("p1")
+        ax.set_ylabel("p2")
+        plt.colorbar(im)
+        ax.set_title(f"Session {i} {title}: {fnames[i]}")
         plt.show()
 
 def main():
@@ -41,12 +51,24 @@ def main():
     mpl.rcParams['axes.prop_cycle'] = cycler(color=['b', 'r', 'g', 'y'])
     out_dir = sys.argv[1]
     files = os.listdir(out_dir)
+    lr = [f for f in files if "net_lr" in f]
+    lr = lr[0]
+    plt.plot(np.load(f"{out_dir}/{lr}")[0, :])
+    plt.title("LR")
+    plt.show()
     rewards = [np.load(f"{out_dir}/{f}") for f in files if "session" in f]
     ir_profits = [np.load(f"{out_dir}/{f}") for f in files if "ir_profits" in f]
     ir_prices = [np.load(f"{out_dir}/{f}") for f in files if "ir_prices" in f]
-    plot_reward_array(rewards, nash, coop, "reward", 100, [f for f in files if "session" in f])
-    plot_reward_array(ir_profits, nash, coop, "IR", 1, [f for f in files if "ir_profits" in f], points=True)
-    plot_reward_array(ir_prices, nash_price, coop_price, "IR Prices", 1, [f for f in files if "ir_prices" in f], points=True)
+    grad_norm = [np.load(f"{out_dir}/{f}") for f in files if "grad_norm" in f]
+    actions = [np.load(f"{out_dir}/{f}") for f in files if "actions" in f]
+    values = [np.load(f"{out_dir}/{f}") for f in files if "values" in f]
+    plot_heatmap(actions, "Actions", [f for f in files if "actions" in f])
+    plot_heatmap(values, "Values", [f for f in files if "actions" in f])
+    plot_array_rolling_avg(rewards, nash, coop, "reward", 100, [f for f in files if "session" in f])
+    plot_array_rolling_avg(ir_profits, nash, coop, "IR", 1, [f for f in files if "ir_profits" in f], points=True)
+    plot_array_rolling_avg(ir_prices, nash_price, coop_price, "IR Prices", 1, [f for f in files if "ir_prices" in f], points=True)
+    plot_array_rolling_avg(grad_norm, 0, 0, "Gradient Norm", 1, [f for f in files if "grad_norm" in f], points=False)
+
 
 
 if __name__ == "__main__":
