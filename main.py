5359a6c5c75b1606bc196f77373226265b7892ed
kmfrick <kmfrick98@gmail.com>
2022-04-15 15:18:10 +0200

    Add IR and action saving

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+import sys
+import os
 from tqdm import tqdm
 from collections import deque
 import torch
@@ -10,7 +10,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions import Normal
 
-np.random.seed(50321)
 
 class ActorCriticNetwork(nn.Module):
     def __init__(self, input_size: int, hidden_size: int):
@@ -27,6 +26,7 @@ class ActorCriticNetwork(nn.Module):
         mu, sigma = torch.sigmoid(action)
         value = self.fc2_critic(x)
         return mu, sigma, value
+
 
 def optimize_net(rewards, saved_actions, discount, optimizer):
     # Compute losses
@@ -49,51 +49,104 @@ def optimize_net(rewards, saved_actions, discount, optimizer):
     loss.backward()
     optimizer.step()
 
+def save_actions(net_actor, net_critic, n_agents, c, fpostfix, out_dir):
+    grid_size = 100
+    w = np.linspace(c, c + 2, grid_size)
+
+    for i in range(n_agents):
+        net_actor[i].eval()
+        A = np.zeros([grid_size, grid_size])
+        Q = np.zeros([grid_size, grid_size])
+        for ai, p1 in enumerate(w):
+            for aj, p2 in enumerate(w):
+                state = torch.tensor(np.array([p1, p2]))
+                a, _ , _= net_actor[i](state)
+                #q = net_critic[i](state, a)
+                A[ai, aj] = a.detach().numpy()
+                #Q[ai, aj] = q.detach().numpy()
+        np.save(f"{out_dir}/actions_{fpostfix}_{i}.npy", A)
+        #np.save(f"{out_dir}/values_{fpostfix}_{i}.npy", Q)
+
 def main():
+    if len(sys.argv) < 2:
+        print(f"Usage: {sys.argv[0]} FOLDER_NAME")
+        exit(1)
+    else:
+        out_dir = sys.argv[1]
+        os.makedirs(out_dir, exist_ok=True)
+
     discount = 0.95
     n_episodes = 1000
     n_agents = 2
-    results = np.zeros(n_episodes)
-    hidden_size = 32
     a = np.array([2.0, 2.0])
     a0 = 0
     mu = 0.25
     c = 1
     batch_size = 64
-    net = []
-    net.append(ActorCriticNetwork(n_agents, hidden_size))
-    net.append(ActorCriticNetwork(n_agents, hidden_size))
-    optimizer = [torch.optim.AdamW(n.parameters(), lr=3e-2) for n in net]
-    scheduler = [torch.optim.lr_scheduler.StepLR(o, batch_size, gamma = 0.999) for o in optimizer]
-    cum_rewards = np.zeros([n_agents, 0])
-    state = torch.tensor([1.47] * n_agents)
-    price = np.zeros([n_agents])
-    for i_episode in tqdm(range(n_episodes)):
-        saved_actions = [None] * n_agents
-        rewards = [None] * n_agents
-        for i in range(n_agents):
-            rewards[i] = []
-            saved_actions[i] = []
-        for t in range(batch_size):
+    hidden_size = 32
+
+    seeds = [12654329, 4305430, 3483055, 348203, 2356, 250917, 200822, 151515, 50505, 301524, 12345, 54321, 50321, 9827391,
+             8534101]
+    for session in range(len(seeds)):
+        net = []
+        net.append(ActorCriticNetwork(n_agents, hidden_size))
+        net.append(ActorCriticNetwork(n_agents, hidden_size))
+        optimizer = [torch.optim.AdamW(n.parameters(), lr=3e-2) for n in net]
+        scheduler = [torch.optim.lr_scheduler.StepLR(o, batch_size, gamma=0.999) for o in optimizer]
+        cum_rewards = np.zeros([n_agents, 0])
+        nash_price = 1.47293
+        state = torch.tensor([nash_price] * n_agents)
+        price = np.zeros([n_agents])
+        fpostfix = seeds[session]
+        # Random seeds
+        np.random.seed(seeds[session])
+        torch.manual_seed(seeds[session])
+        ir_periods = 20
+
+        for i_episode in tqdm(range(n_episodes)):
+            saved_actions = [None] * n_agents
+            rewards = [None] * n_agents
             for i in range(n_agents):
-                mean, std, value = net[i](state)
-                m = Normal(mean, std)
-                p = m.sample()
-                price[i] = p + c
-                saved_actions[i].append((m.log_prob(p), value))
-            quant = np.exp((a - price)/mu) / (np.sum(np.exp((a - price)/mu)) + np.exp(a0/mu))
+                rewards[i] = []
+                saved_actions[i] = []
+            for t in range(batch_size):
+                for i in range(n_agents):
+                    mean, std, value = net[i](state)
+                    m = Normal(mean, std)
+                    p = m.sample()
+                    price[i] = p + c
+                    saved_actions[i].append((m.log_prob(p), value))
+                quant = np.exp((a - price) / mu) / (np.sum(np.exp((a - price) / mu)) + np.exp(a0 / mu))
+                profits = (price - c) * quant
+                for i in range(n_agents):
+                    rewards[i].append(profits[i].item())
+                state = torch.tensor(price)
+            for i in range(n_agents):
+                optimize_net(rewards[i], saved_actions[i], discount, optimizer[i])
+                scheduler[i].step()
+            cum_rewards = np.concatenate((cum_rewards, np.array(rewards)), axis=1)
+        # Impulse response
+        ir_profits = np.zeros([n_agents, ir_periods])
+        ir_prices = np.zeros([n_agents, ir_periods])
+        for t in range(ir_periods):
+            for i in range(n_agents):
+                net[i].eval()
+                p, _, _ = net[i](state)
+                price[i] = (p + c).detach().numpy()
+                ir_prices[i, t] = price[i]
+            if (ir_periods / 2) <= t <= (ir_periods / 2 + 3):
+                price[0] = nash_price
+                ir_prices[0, t] = price[0]
+            quant = np.exp((a - price) / mu) / (np.sum(np.exp((a - price) / mu)) + np.exp(a0 / mu))
             profits = (price - c) * quant
-            for i in range(n_agents):
-                rewards[i].append(profits[i].item())
+            ir_profits[:, t] = profits
             state = torch.tensor(price)
-        for i in range(n_agents):
-            optimize_net(rewards[i], saved_actions[i], discount, optimizer[i])
-            scheduler[i].step()
-        cum_rewards = np.concatenate((cum_rewards, np.array(rewards)), axis = 1)
+        np.save(f"{out_dir}/ir_profits_{fpostfix}.npy", ir_profits)
+        np.save(f"{out_dir}/ir_prices_{fpostfix}.npy", ir_prices)
 
-    reward = np.array(cum_rewards)
-    np.save("rewards.npy", reward)
-
+        reward = np.array(cum_rewards)
+        np.save(f"{out_dir}/rewards_{fpostfix}.npy", reward)
+        save_actions(net, None, n_agents, c, f"final_{fpostfix}", out_dir)
 
 
 if __name__ == "__main__":
--- plot.py
+++ plot.py
@@ -1,19 +1,78 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+from cycler import cycler
 import pandas as pd
+import sys
+import os
 
-nash_price = 1.4729273733327568
-coop_price = 1.9249689958811602
 
-nash = 0.22292696
-coop = 0.33749046
+def plot_array_rolling_avg(arr, low_line, high_line, title, rolling_interval, fnames, points=False):
+    tot0 = np.sum(np.array([r[0] for r in arr]), axis=0) / len(arr)
+    tot1 = np.sum(np.array([r[1] for r in arr]), axis=0) / len(arr)
+    tot0_series = pd.Series(tot0).rolling(rolling_interval).mean()
+    tot1_series = pd.Series(tot1).rolling(rolling_interval).mean()
+    if (points):
+        plt.scatter(list(range(len(tot0_series))), tot0_series)
+        plt.scatter(list(range(len(tot1_series))), tot1_series)
+    plt.plot(tot0_series)
+    plt.plot(tot1_series)
+    plt.axhline(low_line)
+    plt.axhline(high_line)
+    plt.title(f"Average {title}")
+    plt.show()
+    for i, r in enumerate(arr):
+        r0_series = pd.Series(r[0]).rolling(rolling_interval).mean()
+        r1_series = pd.Series(r[1]).rolling(rolling_interval).mean()
+        if (points):
+            plt.scatter(list(range(len(r0_series))), r0_series)
+            plt.scatter(list(range(len(r1_series))), r1_series)
+        plt.plot(r0_series)
+        plt.plot(r1_series)
+        plt.axhline(low_line)
+        plt.axhline(high_line)
+        plt.title(f"Session {i} {title}: {fnames[i]}")
+        plt.show()
 
-r = np.load("rewards.npy")
-print(r[0, :10])
-r[0] = (r[0] - nash) / (coop - nash)
-r[1] = (r[1] - nash) / (coop - nash)
 
-plt.plot(pd.Series(r[0]).rolling(100).mean())
-plt.plot(pd.Series(r[1]).rolling(100).mean())
+def plot_heatmap(arr, title, fnames):
+    for i, a in enumerate(arr):
+        ax = plt.subplot()
+        im = ax.imshow(a.reshape([100, 100]), cmap="Reds", extent=(1, 3, 1, 3), aspect="auto", origin="lower")
+        ax.set_xlabel("p0")
+        ax.set_ylabel("p1")
+        plt.colorbar(im)
+        ax.set_title(f"Session {i} {title}: {fnames[i]}")
+        plt.show()
 
-plt.show()
+def load_and_plot(files, out_dir, fname, low, high, rolling_int, title, points=False):
+    arr = [np.load(f"{out_dir}/{f}") for f in files if fname in f]
+    plot_array_rolling_avg(arr, low, high, title, rolling_int, [f for f in files if fname in f], points = points)
+
+def load_and_heatmap(files, out_dir, fname, title):
+    arr = [np.load(f"{out_dir}/{f}") for f in files if fname in f]
+    plot_heatmap(arr, title, [f for f in files if fname in f])
+
+def main():
+    nash_price = 1.4729273733327568
+    coop_price = 1.9249689958811602
+    nash = 0.22292696
+    coop = 0.33749046
+    mpl.rcParams['axes.prop_cycle'] = cycler(color=['b', 'r', 'g', 'y'])
+    out_dir = sys.argv[1]
+    files = os.listdir(out_dir)
+    #load_and_plot(files, out_dir, "actor_loss", 0, 0, 100, "Actor Loss")
+    #load_and_plot(files, out_dir, "actor_norm", 0, 1, 10, "Actor Grad Norm")
+    #load_and_plot(files, out_dir, "critic_loss", 0, 0, 100, "Critic Loss")
+    #load_and_plot(files, out_dir, "critic_norm", 0, 1, 10, "Critic Grad Norm")
+    load_and_plot(files, out_dir, "rewards", nash, coop, 100, "Rewards")
+    #load_and_plot(files, out_dir, "ir_profits", nash, coop, 1, "IR Profits", True)
+    load_and_plot(files, out_dir, "ir_prices", nash_price, coop_price, 1, "IR Prices", True)
+
+    load_and_heatmap(files, out_dir, "actions_final", "Actions")
+    #load_and_heatmap(files, out_dir, "values_final", "Values")
+
+
+
+if __name__ == "__main__":
+    main()
