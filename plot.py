cf964ee1297d38853135ee389fd7574186b4dd6c
kmfrick <kmfrick98@gmail.com>
2022-05-10 11:06:25 +0200

    plot.py: more flexible IR

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import itertools
 import os
 import sys
 
@@ -11,13 +12,14 @@ import torch
 
 from cycler import cycler
 
-from main import MLPActorCritic, scale_price
+from main import SquashedGaussianMLPActor, MLPQFunction, MLPValueFunction, MLPActorCritic, scale_price
 
 
 def plot_heatmap(arr, title, c=1, w=100):
+    plt.tight_layout()
     for i, a in enumerate(arr):
-        ax = plt.subplot()
-        im = ax.imshow(
+        ax = plt.subplot(1, len(arr), i + 1)
+        im = plt.imshow(
             a.reshape([w, w]),
             cmap="Reds",
             extent=(c, 2 * c, c, 2 * c),
@@ -26,9 +28,9 @@ def plot_heatmap(arr, title, c=1, w=100):
         )
         ax.set_xlabel("p0")
         ax.set_ylabel("p1")
-        plt.colorbar(im)
         ax.set_title(f"{title}; Agent {i}")
-        plt.show()
+        plt.colorbar(orientation="horizontal")
+    plt.show()
 
 
 # https://cs231n.github.io/optimization-1/#gradcompute
@@ -71,18 +73,14 @@ def main():
     nash = 0.22292696
     coop = 0.33749046
     N_AGENTS = 2
-    HIDDEN_SIZE = 256
-    MAX_T = int(1e5)
     STARTING_PROFIT_GAIN = 0.9
-    SEEDS = [12345, 54321, 464738, 250917]
+    SEEDS = [250917]
+    T_MAX = [10000, 90000]
     mpl.rcParams["axes.prop_cycle"] = cycler(color=["b", "r", "g", "y"])
     root_dir = sys.argv[1]
 
     ir_periods = 40
     device = torch.cuda.current_device() if torch.cuda.is_available() else "cpu"
-    actor = [
-        MLPActorCritic(N_AGENTS, 1, hidden_sizes=(HIDDEN_SIZE,) * N_AGENTS) for i in range(N_AGENTS)
-    ]
     ai = 2.0
     a0 = 0
     mu = 0.25
@@ -93,63 +91,78 @@ def main():
         pi = (p - c) * q
         return pi
 
-    for fpostfix in SEEDS:
-        out_dir = f"{root_dir}/weights_{fpostfix}"
-
-        for i, a in enumerate(actor):
-            a.pi.load_state_dict(
-                torch.load(
-                    f"{out_dir}/actor_weights_{fpostfix}_t{MAX_T-1}_agent{i}.pth",
-                    map_location=torch.device(device),
-                )
-            )
-        # Create and plot state-action map
-        grid_size = 100
-        w = torch.linspace(c, 2 * c, grid_size, requires_grad=False)
-        A = [torch.zeros([grid_size, grid_size], requires_grad=False) for i in range(N_AGENTS)]
+    for seed in SEEDS:
+        out_dir = f"{root_dir}"
+        rewards = np.load(f"{out_dir}/session_reward_{seed}.npy")
         for i in range(N_AGENTS):
-            for a_i, p1 in enumerate(w):
-                for a_j, p2 in enumerate(w):
-                    state = torch.tensor([[p1, p2]])
-                    a = scale_price(actor[i].act(state, deterministic=True), c)
-                    A[i][a_i, a_j] = a
-        plot_heatmap(A, f"Actions for seed {fpostfix}")
-        # Impulse response
-        price_history = np.zeros([N_AGENTS, ir_periods])
-        state = torch.rand(N_AGENTS).to(device) * c + c
-        for i in range(0, N_AGENTS):
-            state[i] = (coop_price - nash_price) * STARTING_PROFIT_GAIN + nash_price
-        price = state.clone()
-        initial_state = state.clone()
-        # First compute non-deviation profits
-        DISCOUNT = 0.99
-        nondev_profit = 0
-        for t in range(ir_periods):
-            for i in range(N_AGENTS):
-                price[i] = scale_price(actor[i].act(state, deterministic=True), c)
-            if t >= (ir_periods / 2):
-                nondev_profit += Pi(price.numpy())[0] * DISCOUNT ** (t - ir_periods / 2)
-            price_history[:, t] = price
-            state = price
-        # Now compute deviation profits
-        dev_profit = 0
-        state = initial_state.clone()
-        for t in range(ir_periods):
-            for i in range(N_AGENTS):
-                price[i] = scale_price(actor[i].act(state, deterministic=True), c)
-            if t == (ir_periods / 2):
-                price[0] = torch.tensor(grad_desc(Pi, price.numpy(), 0))
-            if t >= (ir_periods / 2):
-                dev_profit += Pi(price.numpy())[0] * DISCOUNT ** (t - ir_periods / 2)
-            price_history[:, t] = price
-            state = price
-        print(f"Non-deviation profits = {nondev_profit:.3f}; Deviation profits = {dev_profit:.3f}")
-        for i in range(N_AGENTS):
-            plt.scatter(list(range(ir_periods)), price_history[i, :])
-        plt.legend(["Deviating agent", "Non-deviating agent"])
-        for i in range(N_AGENTS):
-            plt.plot(price_history[i, :])
+            r_series = pd.Series(rewards[i, :]).ewm(span=np.max(T_MAX)//50)
+            plt.plot(r_series.mean())
+            plt.fill_between(range(len(r_series.mean())), r_series.mean() - r_series.std(), r_series.mean() + r_series.std(), alpha=0.2)
+        plt.axhline(nash)
+        plt.axhline(coop)
         plt.show()
+    with torch.no_grad():
+        for seed, t_max in itertools.product(SEEDS, T_MAX):
+            np.random.seed(seed)
+            torch.manual_seed(seed)
+            actor = [torch.load(f"{out_dir}/actor_weights_{seed}_t{t_max}_agent{i}.pth", map_location=torch.device(device)) for i in range(N_AGENTS)]
+            print(actor)
+            # Create and plot state-action map
+            grid_size = 100
+            w = torch.linspace(c, 2 * c, grid_size, requires_grad=False)
+            A = [torch.zeros([grid_size, grid_size], requires_grad=False) for i in range(N_AGENTS)]
+            print("Computing state-action map...")
+            for i in range(N_AGENTS):
+                for a_i, p1 in enumerate(w):
+                    for a_j, p2 in enumerate(w):
+                        state = torch.tensor([[p1, p2]])
+                        a = scale_price(actor[i](state)[0], c).detach()
+                        print(f"{state} -> {a}")
+                        A[i][a_i, a_j] = a
+            plot_heatmap(A, f"Actions for seed {seed}")
+            print("Computing IRs...")
+            # Impulse response
+            price_history = np.zeros([N_AGENTS, ir_periods])
+            state = torch.rand(N_AGENTS).to(device) * c + c
+            for i in range(0, N_AGENTS):
+                state[i] = (coop_price - nash_price) * STARTING_PROFIT_GAIN + nash_price
+            print(f"Initial state = {state}")
+            price = state.clone()
+            initial_state = state.clone()
+            # First compute non-deviation profits
+            DISCOUNT = 0.99
+            nondev_profit = 0
+            j = 1
+            leg = ["Non-deviating agent"] * N_AGENTS
+            leg[j] = "Deviating agent"
+            for t in range(ir_periods):
+                for i in range(N_AGENTS):
+                    price[i] = scale_price(actor[i](state.unsqueeze(0))[0], c)
+                if t >= (ir_periods / 2):
+                    nondev_profit += Pi(price.numpy())[j] * DISCOUNT ** (t - ir_periods / 2)
+                price_history[:, t] = price.detach()
+                state = price
+            # Now compute deviation profits
+            dev_profit = 0
+            state = initial_state.clone()
+            for t in range(ir_periods):
+                for i in range(N_AGENTS):
+                    price[i] = scale_price(actor[i](state.unsqueeze(0))[0], c)
+                if t == (ir_periods / 2):
+                    br = grad_desc(Pi, price.numpy(), j)
+                    price[j] = torch.tensor(br)
+                if t >= (ir_periods / 2):
+                    dev_profit += Pi(price.numpy())[j] * DISCOUNT ** (t - ir_periods / 2)
+                price_history[:, t] = price
+                state = price
+            dev_gain = (dev_profit / nondev_profit - 1) * 100
+            print(f"Non-deviation profits = {nondev_profit:.3f}; Deviation profits = {dev_profit:.3f}; Deviation gain = {dev_gain:.3f}%")
+            for i in range(N_AGENTS):
+                plt.scatter(list(range(ir_periods)), price_history[i, :])
+            plt.legend(leg)
+            for i in range(N_AGENTS):
+                plt.plot(price_history[i, :])
+            plt.show()
 
 
 if __name__ == "__main__":
